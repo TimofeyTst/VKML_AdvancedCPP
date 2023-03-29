0d6fe1b4d051f2ca789938a750384d54f882955f
TimofeyTst <yarik.mist@gmail.com>
2023-03-29 19:53:00 +0300

    Sigmentation fault

--- include/IOperation.hpp
+++ include/IOperation.hpp
@@ -1,4 +1,110 @@
 #pragma once
 #include <iostream>
+#include <fstream>
 
-void print();
+class IOperation {
+public:
+    IOperation() : next_operation_(nullptr) {}
+    IOperation(IOperation * op) : next_operation_(op) {}
+    virtual void ProcessLine(const std::string& str) = 0;
+    virtual void HandleEndOfInput() = 0;
+    virtual void SetNextOperation(IOperation* operation) = 0;
+    virtual ~IOperation() {
+        if(next_operation_){
+            delete next_operation_;
+        }
+    }
+    IOperation* next_operation_;
+};
+
+class EchoOperation : public IOperation {
+public:
+    EchoOperation() : IOperation(nullptr) {}
+    EchoOperation(std::string& str, IOperation* operation = nullptr) : IOperation(operation), str_(str) {}
+
+    void ProcessLine(const std::string& str) override {
+        std::cout << str << std::endl;
+    }
+
+    void HandleEndOfInput() override {
+        ProcessLine(str_);
+        if(next_operation_) {
+            next_operation_->HandleEndOfInput();
+        }
+    }
+
+    void SetNextOperation(IOperation* operation) override {
+        next_operation_ = operation;
+    }
+
+private:
+    std::string str_;
+};
+
+
+class CatOperation : public IOperation {
+public:
+    CatOperation() : IOperation(nullptr) {}
+    CatOperation(std::string& filename, IOperation* next_operation = nullptr) : IOperation(next_operation), filename_(filename) {}
+    
+    void ProcessLine(const std::string& str) override {
+        std::ifstream file_(str);
+        if (!file_.is_open()) {
+            throw std::runtime_error("Cannot open file: " + str);
+        }
+        // читаем строки из файла и выводим их
+        std::string line;
+        while (std::getline(file_, line)) {
+            std::cout << line << std::endl;
+        }
+    }
+    
+    void HandleEndOfInput() override {
+        ProcessLine(filename_);
+        if (next_operation_) {
+            next_operation_->HandleEndOfInput();
+        }
+    }
+    
+    void SetNextOperation(IOperation* operation) override {
+        next_operation_ = operation;
+    }
+    
+private:
+    std::string filename_;
+};
+
+class OperationsList {
+public:
+    OperationsList() : head_(nullptr), tail_(nullptr) {}
+
+    void AddOperation(IOperation* op) {
+        if (!head_) {
+            head_ = op;
+            tail_ = op;
+        } else {
+            tail_->SetNextOperation(op);
+            tail_ = op;
+        }
+    }
+
+    void RunOperations() {
+        if (head_) {
+            head_->HandleEndOfInput();
+        }
+    }
+
+    virtual ~OperationsList() {
+        IOperation* cur = head_;
+        IOperation* prev;
+        while (cur) {
+            prev = cur;
+            cur = cur->next_operation_;
+            delete prev;
+        }
+    }
+
+private:
+    IOperation* head_;
+    IOperation* tail_;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
 #include "IOperation.hpp"
 
+
 int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
-    print();
+    std::string input = "echo million$ | cat static/text.txt | cat static/zarplata.txt";
+
+    OperationsList list;
+
+    std::string delimiter = " | ";
+    size_t pos = 0;
+    std::string token;
+    while ((pos = input.find(delimiter)) != std::string::npos) {
+        token = input.substr(0, pos);
+        input.erase(0, pos + delimiter.length());
+
+        if (token.find("echo") == 0) {
+            std::string str = token.substr(5);
+            list.AddOperation(new EchoOperation(str));
+        } else if (token.find("cat") == 0) {
+            std::string file = token.substr(4);
+            list.AddOperation(new CatOperation(file));
+        }
+    }
+
+    if (!input.empty()) {
+        if (input.find("echo") == 0) {
+            std::string str = input.substr(5);
+            list.AddOperation(new EchoOperation(str));
+        } else if (input.find("cat") == 0) {
+            std::string file = input.substr(4);
+            list.AddOperation(new CatOperation(file));
+        }
+    }
+
+    list.RunOperations();
+
+    return 0;
 }
