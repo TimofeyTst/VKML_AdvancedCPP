74104b39d42c6dc5f0039e4a59ed45b578851170
TimofeyTst <yarik.mist@gmail.com>
2023-04-05 22:32:27 +0300

    some clean code

--- src/WcOperation.cpp
+++ src/WcOperation.cpp
@@ -7,10 +7,12 @@ void WcOperation::ProcessLine(const std::string& str) {
     int bytes = 0;
     while (iss >> filename) { // Читаем имена файлов из потока
         std::ifstream file(filename, std::ios::binary);
-        if (file) { // Проверяем, успешно ли открылся файл
-            file.seekg(0, std::ios::end);
-            bytes += file.tellg();
+        if (!file.is_open()) {
+            throw std::runtime_error("Cannot open file: " + filename);
         }
+
+        file.seekg(0, std::ios::end);
+        bytes += file.tellg();
     }
     // Выводим результат в стандартный поток вывода
     std::cout << "Total bytes: " << bytes << std::endl;
